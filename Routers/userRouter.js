c32aba76e1e89d2199086b936df1fe61b9ca8d2e
varatharaj <varathan2512002@gmail.com>
2023-12-15 09:45:33 +0530

    inital commit

---
+++ Routers/userRouter.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const userModel = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try {
+        var emailExist = await userModel.findOne({ email: req.body.email });
+        if (emailExist) {
+            return res.status(400).json({ message: "   email already exist :(   " });
+        }
+
+        //pasword hash
+        var hash = await bcrypt.hash(req.body.password, 10);
+
+        const user = new userModel({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        var data = await user.save();
+        res.status(200).json({message: 'Account Created Successfully :) ', data: data});
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    var userData = await userModel.findOne({ email: req.body.email });
+    if (!userData) {
+        return res.status(400).json({ message: "  email not exist :(  " });
+    }
+    try {
+        var validPassword = await bcrypt.compare(req.body.password, userData.password);
+        if (!validPassword) {
+            return res.status(400).json({ message: "  password not valid :(  " });
+        }
+        var userToken = jwt.sign({ email: userData.email, name: userData.name }, 'periyaRagasiyam');
+        res.header('auth', userToken).json({ message: 'Login successful :)', data: userToken });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
+const validUser = (req, res, next) => {
+    var token = req.header('auth');
+    req.token = token
+    next();
+}
+
+router.get('/getAll', validUser, async (req, res) => {
+
+    jwt.verify(req.token, 'periyaRagasiyam', async (err, data) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            try {
+                const Users = await userModel.find().select(['-password']);
+                res.status(200).json(Users)
+            } catch (error) {
+                res.status(500).json(error);
+            }
+        }
+    });
+});
+
+
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const userRoute = require('./Routers/userRouter');
+const morgan = require('morgan');
+var cors = require('cors');
+
+const app = express();
+const MONGO_URL = process.env.MONGO_URL
+const PORT = process.env.PORT || 3001
+const FRONTEND = process.env.FRONTEND
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+app.use('/api', userRoute);
+
+app.get('/', (req, res) => {
+    res.send('This is Vasanth Node Js API');
+});
+
+mongoose.set("strictQuery", false);
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        console.log('connected to mongo db');
+        app.listen(PORT, () => {
+            console.log(`Port working in http://localhost:${PORT}`);
+            console.log(`current working Cors is ${FRONTEND}`);
+        });
+    }).catch((error) => {
+        console.log(error);
+    });
